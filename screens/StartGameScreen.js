99770ac9ce8a2b8f28bb4e9492cf7dbbb71bacd4
HamnaSiddiqui <hamnas200@gmail.com>
2023-05-27 20:47:04 +0500

    React native Gaming app Starter files

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,42 @@
+import { View, TextInput, StyleSheet } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
+
+function StartGameScreen() {
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
+    </View>
+  );
+}
+
+export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    padding: 16,
+    marginTop: 100,
+    marginHorizontal: 24,
+    backgroundColor: "#4e0329",
+    borderRadius: 8,
+    elevation: 4,
+  },
+  numberInput: {
+    width: 50,
+    textAlign: "center",
+    height: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+  },
+});
