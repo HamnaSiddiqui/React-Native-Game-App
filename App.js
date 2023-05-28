7cdbc25020cf4ef0f96054748006658889064eea
HamnaSiddiqui <hamnas200@gmail.com>
2023-05-28 21:17:07 +0500

    linear gradient

--- App.js
+++ App.js
@@ -1,16 +1,30 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, ImageBackground } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 
 import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
+  const img = require("./assets/images/background.png");
   return (
-    <LinearGradient style={styles.rootScreen}>
-      <StartGameScreen />
+    <LinearGradient colors={["#4e0329", "#ddb52f"]} style={styles.rootScreen}>
+      <ImageBackground
+        source={img}
+        resizeMode="cover"
+        style={styles.rootScreen}
+        imageStyle={styles.backgroundImage}
+      >
+        <StartGameScreen />
+      </ImageBackground>
     </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
-  rootScreen: { flex: 1, backgroundColor: "ddb52f" },
+  rootScreen: {
+    flex: 1,
+    backgroundColor: "ddb52f",
+  },
+  backgroundImage: {
+    opacity: 0.25,
+  },
 });
